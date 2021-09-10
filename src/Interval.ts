6672a8205bd9b56aee92c16d10429fc04b1f7580
刘星宇 <scliuxingyu@icloud.com>
2021-09-10 16:07:18 +0800

    interval 测试完成

--- src/Interval.ts
+++ src/Interval.ts
@@ -1,41 +1,47 @@
-const format = (arr) => {
+const format = ( arr , endTime ) => {
+    const useArr=arr.slice();
     const re = [];
+    if ( useArr.length % 2 !== 0 ) useArr.push( endTime );
     let i = 0;
-    while (i < arr.length) {
-        re.push([arr[i], arr[++i]]);
+    while ( i < useArr.length ) {
+        if ( useArr[ i ] > useArr[ i + 1 ] ) {
+            i = Infinity;
+            break;
+        }
+        re.push( [ useArr[ i ] , useArr[ ++i ] ] );
         i++;
     }
     return re;
 };
-const getTime = (time, intervals) => {
+const getTime = ( time , intervals ) => {
     let d = 0;
-    if (time) {
-        intervals.some(([start, end]) => {
-            if (time < start) {
+    if ( time ) {
+        intervals.some( ( [ start , end ] ) => {
+            if ( time < start ) {
                 return true;
             }
-            if (time > end) {
+            if ( time > end ) {
                 d += end - start;
                 return false;
             }
             d += time - start;
             return true;
-        });
+        } );
     }
     return d;
 };
 
 export class Interval {
     private hasEndTime;
-    private readonly intervals: [ number , number ][];
+    private intervals: [ number , number ][];
+    private readonly _initIntervals;
     
     constructor( intervals , endTime ) {
-        if ( intervals.length % 2 !== 0 ) {
-            intervals.push( endTime );
-        } else {
+        if ( intervals.length % 2 === 0 ) {
             this.hasEndTime = true;
         }
-        this.intervals = format( intervals );
+        this._initIntervals = intervals.slice();
+        this.intervals = format( intervals , endTime );
     }
     
     get endTime() {
@@ -62,7 +68,7 @@ export class Interval {
         let pauseTime;
         const { intervals } = this;
         const last = intervals.length - 1;
-        if ( intervals.length === 1 ) {return false;}
+        if ( intervals.length === 1 ) {return undefined;}
         intervals.some(
             ( value , index , arr ) => {
                 if ( index === last ) {
@@ -79,8 +85,11 @@ export class Interval {
     setEndTime( time ) {
         const { hasEndTime , intervals } = this;
         if ( !hasEndTime ) {
-            intervals[ intervals.length - 1 ][ 1 ] = time;
+            const endTime = intervals[ intervals.length - 1 ][ 1 ];
+            if ( endTime <= time ) intervals[ intervals.length - 1 ][ 1 ] = time;
+            else {
+                this.intervals = format( this._initIntervals , time );
+            }
         }
     }
 }
-
--- src/interval.test.ts
+++ src/interval.test.ts
@@ -1,3 +1,37 @@
 const { Interval } = require( './Interval' );
-const interv1 = new Interval( [ 100 , 200 , 400 , 500 ] , 500 );
-test( 'include' , () => expect( interv1.include( 150 ) ).toBe( true ) );
+describe( 'interval test' , () => {
+    test( 'interval with pause and resume' , () => {
+        const interv = new Interval( [ 100 , 200 , 400 , 500 ] , 500 );
+        expect( interv.include( 150 ) ).toBe( true );
+        expect( interv.overTime( 499 ) ).toBe( false );
+        expect( interv.overTime( 500 ) ).toBe( true );
+        expect( interv.inPaused( 300 ) ).toBe( 200 );
+        expect( interv.inPaused( 150 ) ).toBe( undefined );
+        expect( interv.inPaused( 450 ) ).toBe( undefined );
+        expect( interv.getTime( 150 ) ).toBe( 50 );
+        expect( interv.getTime( 250 ) ).toBe( 100 );
+        expect( interv.getTime( 450 ) ).toBe( 150 );
+    } );
+    test( 'interval without pause' , () => {
+        const interv = new Interval( [ 100 ] , 500 );
+        expect( interv.include( 50 ) ).toBe( false );
+        expect( interv.overTime( 499 ) ).toBe( false );
+        expect( interv.overTime( 500 ) ).toBe( true );
+        expect( interv.inPaused( 300 ) ).toBe( undefined );
+        expect( interv.getTime( 350 ) ).toBe( 250 );
+        expect( interv.endTime ).toBe( 500 );
+    } );
+    test( 'interval endTime set' , () => {
+        const interv = new Interval( [ 100 , 200 , 300 ] , 500 );
+        interv.setEndTime( 600 );
+        expect( interv.endTime ).toBe( 600 );
+        interv.setEndTime( 400 );
+        expect( interv.endTime ).toBe( 400 );
+        interv.setEndTime( 300 );
+        expect( interv.include( 300 ) ).toBe( true );
+        expect( interv.include( 299 ) ).toBe( false );
+        expect( interv.include( 301 ) ).toBe( false );
+        interv.setEndTime( 250 );
+        expect( interv.endTime ).toBe( 200 );
+    } );
+} );
